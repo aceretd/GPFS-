0834ab99d95b842454fe9ccc4601a3450b036c79
Mark Baldwin Martinez <mbmartinez@exist.com>
2017-07-29 11:56:31 +0800

    UI Initial

---
+++ gpfs-gpfs/src/main/java/com/gpfs/answer/ReconciliationTable.java
@@ -0,0 +1,48 @@
+package com.gpfs.answer;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity(name = "reconciliation_table")
+public class ReconciliationTable extends ScheduleTable {
+
+	@Column(name = "total_equity_ld")
+	private BigDecimal totalEquityLastDay;
+
+	@Column(name = "total_equity_fd")
+	private BigDecimal totalEquityFirstDay;
+
+	@Column(name = "coc_ld")
+	private BigDecimal capitalizationOfCostsLastDay;
+
+	@Column(name = "coc_fd")
+	private BigDecimal capitalizationOfCostsFirstDay;
+
+	public BigDecimal getTotalEquityLastDay() {
+		return totalEquityLastDay;
+	}
+	public void setTotalEquityLastDay(BigDecimal totalEquityLastDay) {
+		this.totalEquityLastDay = totalEquityLastDay;
+	}
+	public BigDecimal getTotalEquityFirstDay() {
+		return totalEquityFirstDay;
+	}
+	public void setTotalEquityFirstDay(BigDecimal totalEquityFirstDay) {
+		this.totalEquityFirstDay = totalEquityFirstDay;
+	}
+	public BigDecimal getCapitalizationOfCostsLastDay() {
+		return capitalizationOfCostsLastDay;
+	}
+	public void setCapitalizationOfCostsLastDay(BigDecimal capitalizationOfCostsLastDay) {
+		this.capitalizationOfCostsLastDay = capitalizationOfCostsLastDay;
+	}
+	public BigDecimal getCapitalizationOfCostsFirstDay() {
+		return capitalizationOfCostsFirstDay;
+	}
+	public void setCapitalizationOfCostsFirstDay(BigDecimal capitalizationOfCostsFirstDay) {
+		this.capitalizationOfCostsFirstDay = capitalizationOfCostsFirstDay;
+	}
+
+}
---
+++ gpfs-gpfs/src/main/java/com/gpfs/answer/ScheduleTable.java
@@ -0,0 +1,22 @@
+package com.gpfs.answer;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class ScheduleTable {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
